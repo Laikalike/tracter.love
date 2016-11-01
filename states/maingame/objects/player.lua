006b5b4ebd555e8b5d28ee8396d84b6ba3ccc1c5
Laika <fraktalsyt@gmail.com>
2016-11-01 16:06:34 +0000

    Added physics functionality for all entites
    Added a physics class that deals with gravity, collisions and velocity,
    rewritten from old player collision. Compacted the player entity a bit.
    4.06AM, 1.11.16

--- states/maingame/objects/player.lua
+++ states/maingame/objects/player.lua
@@ -1,25 +1,17 @@
 -- Player class, inherits off entity, camera is centered on it.
 local Player = {}
-
-require "states/maingame/tools/camera"
-require "states/maingame/tools/physics_helper"
-local rect = require "states/maingame/objects/eRect"
-
-local floor = math.floor
+require "states/maingame/world_physics/physics"
 
 local tiles = tlm.tiles[1]
 
 function Player:new(x,y)
-
 
   local player = require("states/maingame/objects/entity"):new(x, y, 70, 70, nil, nil, "player")
 
   function player:load()
     gameLoop:addLoop(self)
     renderer:addRenderer(self)
-
-    self.on_ground = false
-    self.gravity = 500
+    physics_init(self,500)
   end
 
   local key = love.keyboard.isDown
@@ -27,37 +19,7 @@ function Player:new(x,y)
   function player:tick(dt)
     camera:goto_point(self.pos)
 
-    self.vel.y = self.vel.y + (self.gravity * dt * 10)
-    self.dir.y = 1
-
-    local x = floor(self.pos.x / 70) - 1
-    local y = floor(self.pos.y / 70) - 1
-    local w = 3
-    local h = 3
-
-    if x < 1 then x = 1 end
-    if y < 1 then y = 1 end
-
-    for i = y,y+h do
-      for j = x,x+w do
-        local tile = tiles[i][j]
-        if tile == nil then goto cont end
-
-        local box = rect:new(self.pos.x, self.pos.y + self.vel.y * dt, self.size.x, self.size.y)
-
-        local coll, rect = rect_collision(box,tile)
-        if coll then
-          self.vel.y = 0
-          self.dir.y = 0
-
-          if self.pos.y + self.size.y > rect.pos.y then
-            self.pos.y = rect.pos.y - self.size.y
-          end
-        end
-
-        ::cont::
-      end
-    end
+    physics_gravity(self, dt)
 
     if key("a") then
       self.dir.x = -1
@@ -69,10 +31,10 @@ function Player:new(x,y)
       self.vel.x = 500
     end
 
+    physics_update(self,tiles,dt)
+
     if key("space") then
-      if self.vel.y < 10 and self.vel.y > -10 then
-        self.vel.y =  -self.gravity * 3
-      end
+      physics_jump(self)
     end
 
     self.pos.x = self.pos.x + (self.vel.x * dt) * self.dir.x
---
+++ states/maingame/world_physics/physics.lua
@@ -0,0 +1,98 @@
+local floor = math.floor
+require "states/maingame/tools/camera"
+require "states/maingame/tools/physics_helper"
+local rect = require "states/maingame/objects/eRect"
+
+function physics_init(obj,gravity)
+  obj.on_ground = false
+  obj.gravity   = gravity or 500
+end
+
+function physics_gravity(obj,dt)
+  obj.vel.y = obj.vel.y + (obj.gravity * dt * 10)
+  obj.dir.y = 1
+end
+
+function physics_jump(obj)
+  if obj.vel.y < 10 and obj.vel.y > -10 and obj.on_ground then
+    obj.vel.y =  -obj.gravity * 3
+    obj.on_ground = false
+  end
+end
+
+function physics_update(obj,tiles,dt)
+  local x = floor(obj.pos.x / 70) - 1
+  local y = floor(obj.pos.y / 70) - 1
+  local w = 3
+  local h = 3
+
+  if x < 1 then x = 1 end
+  if y < 1 then y = 1 end
+
+  for i = y,y+h do
+    for j = x,x+w do
+      local tile = tiles[i][j]
+      if tile == nil then goto cont end
+
+
+      local box = rect:new(obj.pos.x + ( obj.vel.x * dt * obj.dir.x ) , obj.pos.y + ( obj.vel.y * dt ) , obj.size.x , obj.size.y )
+
+      local coll, rect = rect_collision(box,tile)
+      if coll then
+        obj.vel.y = 0
+        obj.dir.y = 0
+
+
+        if obj.pos.y + obj.size.y / 2 < rect.pos.y + rect.size.y / 2 then
+
+          if box.pos.y + box.size.y > rect.pos.y and
+             obj.pos.y + obj.size.y < rect.pos.y + 35 then
+
+            obj.on_ground = true
+            obj.pos.y = rect.pos.y - obj.size.y
+          end
+
+        else
+          if obj.pos.y > rect.pos.y + rect.size.y - 35 then
+            obj.vel.y = 0
+            obj.pos.y = rect.pos.y + rect.size.y + 1
+
+            goto skip_x
+          end
+        end
+
+
+
+        if obj.pos.x + obj.size.x / 2 < rect.pos.x + rect.size.x  / 2 then
+
+          if box.pos.x + box.size.x > rect.pos.x and
+             obj.pos.y + obj.size.y > rect.pos.y then
+
+            obj.vel.x = 0
+            obj.dir.x = 0
+
+            obj.pos.x = rect.pos.x - obj.size.x
+          end
+
+
+        else
+
+          if box.pos.x < rect.pos.x + rect.size.x and
+             obj.pos.y + obj.size.y > rect.pos.y then
+
+            obj.vel.x = 0
+            obj.dir.x = 0
+
+            obj.pos.x = rect.pos.x + rect.size.x
+          end
+
+        end
+
+
+        ::skip_x::
+      end
+
+      ::cont::
+    end
+  end
+end
