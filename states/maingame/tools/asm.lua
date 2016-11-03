6b705c879f68e12bf0309a1d21597e11e03c7bde
Laikalike <fraktalsyt@gmail.com>
2016-11-03 21:48:43 +0100

    Added remove function to the asset manager.
    ASM:remove can now be called to remove items from the asset manager
    class.
    
    21.48PM (UTC+1:00), 3.11.16

--- states/maingame/tools/asm.lua
+++ states/maingame/tools/asm.lua
@@ -10,6 +10,14 @@ function ASM:add(asset,id)
   table.insert(self.assets,a)
 end
 
+function ASM:remove(id)
+  for i,a in ipairs(self.assets) do
+    if a.id == id then
+      table.remove(a.asset)
+    end
+  end
+end
+
 function ASM:get(id)
   for i,a in ipairs(self.assets) do
     if a.id == id then
